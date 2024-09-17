1d7b2f543169d1512fe8c13e1157480554a51c88
eshenka <yuliaolegyureva@gmail.com>
2024-09-17 17:03:49 +0700

    lect

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,8 +1,28 @@
 use std::mem::MaybeUninit;
 use std::net::{Ipv4Addr, SocketAddrV4};
+use rand::Rng;
 use socket2::{Socket, Domain, Type, Protocol, SockAddr};
 
-fn send_package(socket: &Socket, addr: &SockAddr) {
+fn send_package(socket: &Socket, addr: &SockAddr, id: &u64) {
+    let mut buf: Vec<u8> = Vec::from("secret message".as_bytes());
+
+    buf.extend(id);
+
+    let send_result = socket.send_to("secret message".as_bytes(), addr);
+
+    let size = match send_result {
+        Ok(size) => size,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::WouldBlock => 0,
+            _ => panic!("Failed sending message"),
+        }
+    };
+}
+
+fn _send_package(socket: &Socket, addr: &SockAddr, id: &u64) {
+    let mut buf: Vec<u8> = Vec::from("secret message".as_bytes());
+
+
     let send_result = socket.send_to("secret message".as_bytes(), addr);
 
     let size = match send_result {
@@ -13,12 +33,8 @@ fn send_package(socket: &Socket, addr: &SockAddr) {
         }
     };
 
-    if size != 0 {
-        println!("sended");
-    }
 }
-
-fn recv_package(socket: &Socket) {
+fn recv_package(socket: &Socket, id: &u64) {
     let mut buf: [MaybeUninit<u8>; 256] = [const { MaybeUninit::uninit() }; 256];
     let recv_result = socket.recv_from(&mut buf);
 
@@ -26,8 +42,10 @@ fn recv_package(socket: &Socket) {
     let addr_def_init = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
     let addr_def = SockAddr::from(addr_def_init);
 
+    let buf: &[u8; 256] = unsafe { std::mem::transmute(&buf) };
 
-    let (bytes, _addr) = match recv_result {
+
+    let (bytes, addr) = match recv_result {
         Ok(res) => res,
         Err(error) => match error.kind() {
             std::io::ErrorKind::WouldBlock => (size, addr_def),
@@ -35,8 +53,14 @@ fn recv_package(socket: &Socket) {
         }
     };
 
+
     if bytes != 0 {
-        println!("{:?}", buf);
+        let message = &buf[0..14];
+        let recv_id = &buf[14..22];
+        
+        if message == "secret message".as_bytes() && recv_id != id.as_bytes() {
+            println!("Got message from: {:?}", addr);
+        }
     }
 
 }
@@ -55,8 +79,12 @@ fn main() {
     socket.bind(&addr)
         .expect("Failed bind");
 
+    let mut rng = rand::thread_rng();
+    let shift = 0x0101010101010101;
+    let id: u64 = rng.gen_range(0..u64::MAX - shift) + shift;
+
     loop {
-        send_package(&socket, &addr);
+        send_package(&socket, &addr, &id);
         recv_package(&socket);
     }
 }
