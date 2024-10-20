a0fe255e71d1758b84e55f9b3d1f55a33820e9a3
Yulia <yuliaolegyureva@gmail.com>
2024-10-20 17:01:43 +0700

    1st method is working

--- lab3py/app/handlers.py
+++ lab3py/app/handlers.py
@@ -1,8 +1,15 @@
 import app.keyboard as keyboards
 
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery, FSInputFile
+
+import requests
+
+from config import GRAPHHOPPER_API_KEY
 
 router = Router()
 
@@ -14,4 +21,24 @@ async def command_start(message: Message):
 
 @router.message()
 async def process_location(msg: Message):
-    await msg.answer("You want me to find " + msg.text + " for you. I can do that!")
+    # await msg.answer("You want me to find " + msg.text + " for you. I can do that!")
+    url = "https://graphhopper.com/api/1/geocode"
+
+    query = {
+        "q": msg.text,
+        "limit": "10",
+        "provider": "default",
+        "key": GRAPHHOPPER_API_KEY
+    }
+
+    response = requests.get(url, params=query)
+    answer = response.json()['hits']
+
+    places = []
+    for place in answer:
+        name = "{}, страна: {}".format(place['name'], place['country'])
+        places.append([InlineKeyboardButton(text=name, callback_data="place-btn-chosen")])
+
+    places_btns = InlineKeyboardMarkup(inline_keyboard=places)
+
+    await msg.answer(text="Пожалуйста, выберите подходящее место", reply_markup=places_btns)
